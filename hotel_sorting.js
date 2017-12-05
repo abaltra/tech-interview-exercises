e9582fa3a7585a65dcc9b8834496dd1aea37291a
Alejandro Baltra <abaltra@gmail.com>
2017-12-05 19:21:29 -0300

    finished hotel sorting

--- hotel_sorting.js
+++ hotel_sorting.js
@@ -1,3 +1,70 @@
 /*
-You have a list of words and a list of hotel reviews, try to sort the hotels based on the number of words from the first list that is exist in the each review. if an hotel has maximum number of 4 words then it should be the highest in the sorting..
+	You have a list of words and a list of hotel reviews, try to sort the hotels based on the number of words from the first list that is exist in the each review. if an hotel has maximum number of 4 words then it should be the highest in the sorting..
+
+	dict: ['good', 'great', 'cheap', 'awesome']
+	reviews: [
+	{
+		hotel_id: 'a',
+		review: 'it is pretty good'
+	},
+	{
+		hotel_id: 'b',
+		review: 'nah, it sucks'
+	},
+	{
+		hotel_id: 'a',
+		review: 'an awesome experience!'
+	}
+]
 */
+
+function sort_hotels(words, hotels) {
+	let d = {};
+	words.forEach(word => {
+		d[word] = 1;
+	});
+
+	let graded_hotels = {};
+
+	hotels.forEach(hotel => {
+		if (!graded_hotels[hotel.hotel_id]) {
+			graded_hotels[hotel.hotel_id] = {
+				score: 0
+			}
+		}
+
+		hotel.review.split(' ').forEach(word => {
+			if (d[word]) {
+				graded_hotels[hotel.hotel_id].score ++;
+			}
+		});
+	});
+
+	let ret = [];
+	Object.keys(graded_hotels).forEach(hotel_id => {
+		ret.push(hotel_id)
+	});
+
+	return ret.sort((a, b) => {
+		if (graded_hotels[a].score === graded_hotels[b].score) return 0;
+		if (graded_hotels[a].score > graded_hotels[b].score) return -1;
+		return 1;
+	});
+}
+
+let dict = ['good', 'great', 'cheap', 'awesome'];
+let reviews = [
+{
+	hotel_id: 'a',
+	review: 'it is pretty good'
+},
+{
+	hotel_id: 'b',
+	review: 'nah, it sucks'
+},
+{
+	hotel_id: 'a',
+	review: 'an awesome experience!'
+}];
+
+console.log(sort_hotels(dict, reviews));
