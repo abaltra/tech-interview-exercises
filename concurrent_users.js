0163e9b6591045907ed076c8283006f111562ef2
Alejandro Baltra <abaltra@gmail.com>
2017-12-05 17:37:08 -0300

    concurrent users. removed duplicate problem

--- concurrent_users.js
+++ concurrent_users.js
@@ -13,3 +13,51 @@ Input:
 Output:
 3
 */
+
+function max_concurrent_users(list) {
+	let starts = [];
+	let ends = [];
+
+	list.forEach(log => {
+		starts.push(log.login);
+		ends.push(log.logout);
+	});
+
+	let sorter = (a, b) => {
+		if (a == b) return 0;
+		if (a < b) return -1;
+		else return 1;
+	}
+
+	starts.sort(sorter);
+	ends.sort(sorter);
+
+	let i = 0;
+	let j = 0;
+	let count = 0;
+	let max = 0;
+
+	while (i < starts.length && j < ends.length) {
+		if (starts[i] < ends[j]) {
+			count++;
+			i++;
+			max = Math.max(count, max);
+		} else {
+			count--;
+			j++;
+		}
+	}
+
+	return max;
+
+}
+
+let input = [
+  {user: 'A', login: 1, logout: 3},
+  {user: 'B', login: 3, logout: 4},
+  {user: 'C', login: 1, logout: 2},
+  {user: 'D', login: 123123123, logout: 987987987},
+  {user: 'E', login: 1, logout: 3}
+];
+
+console.log(max_concurrent_users(input));
--- timeslot_overlap.js
+++
@@ -1,3 +0,0 @@
-/*
-You have time-stamps (mentioned as integers), which represents phone call begin and end to booking.com call center. find the number of overlaps in calls that suggests to add more call center agents
-*/
