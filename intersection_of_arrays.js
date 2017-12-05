a651727d6de23dacd4e82f0ca171a79f4c20b705
Alejandro Baltra <abaltra@gmail.com>
2017-12-05 18:46:11 -0300

    finished intersection of arrays

--- intersection_of_arrays.js
+++ intersection_of_arrays.js
@@ -1,3 +1,34 @@
 /*
-Write algorithm to compute the intersection of two arrays. What is the time complexity of this algorithm (using the big O notation)?
+	Write algorithm to compute the intersection of two arrays. What is the time complexity of this algorithm (using the big O notation)?
 */
+
+function intersection(arr1, arr2) {
+	let intersect = [];
+	let f = (a, b) => {
+		if (a == b) return 0;
+		if (a < b) return -1;
+		else return 1;
+	};
+
+	arr1.sort(f);
+	arr2.sort(f);
+
+	let i = 0;
+	let j = 0;
+
+	while (i < arr1.length && j < arr2.length) {
+		if (arr1[i] == arr2[j]) {
+			intersect.push(arr1[i]);
+			i++;
+			j++;
+		} else if (arr1[i] > arr2[j]) {
+			j++;
+		} else {
+			i++;
+		}
+	}
+
+	return intersect;
+}
+
+console.log(intersection([1,2,3,4], [5,3,1,2,7,6,9,4]))
