12e945dcf767fd2a55fc7343301ce1d422c056c7
Alejandro Baltra <abaltra@gmail.com>
2017-12-05 17:47:18 -0300

    completed anagrams set

--- anagrams_set.js
+++ anagrams_set.js
@@ -1,3 +1,35 @@
 /*
-Given an array of words as NSStrings you should return another array of NSStrings, each containing words that are mutual anagrams
+	Given an array of words as Strings you should return another array of Strings, each containing words that are mutual anagrams
 */
+
+function find_anagrams(list) {
+	let word_map = {};
+	let has_anagrams = [];
+	list.forEach(word => {
+		let value = word_to_ascii(word);
+		if (!word_map[value]) {
+			word_map[value] = new Set([]);
+		}
+		word_map[value].add(word);
+	});
+	let keys = Object.keys(word_map);
+	let ret = [];
+	keys.forEach(key => {
+		if (word_map[key].size > 1) {
+			ret.push(word_map[key]);
+		}
+	});
+
+	return ret;
+}
+
+function word_to_ascii(word) {
+	let value = 0;
+	for (let i = 0; i < word.length; i++) {
+		value += word[i].charCodeAt(0);
+	}
+
+	return value;
+}
+
+console.log(find_anagrams(['one', 'two', 'neo', 'wto', 'oen', 'wat']))
