d4a059bdc2ff17f1a43f8063e0b1bb42d5194711
Alejandro Baltra <abaltra@gmail.com>
2017-12-06 18:01:21 -0300

    finished name chain

--- name_chain.js
+++ name_chain.js
@@ -5,3 +5,40 @@ input: [Luis, Hector, Selena, Emmanuel, Amish]
 output: [Emmanuel, Luis, Selena, Amish, Hector]
 
 */
+
+function sorter(names) {
+	let starts = {};
+	let ends = {};
+	let total = names.length;
+
+	for (let i = 0; i < names.length; i++) {
+		let name = names[i];
+		starts[name[0].toLowerCase()] = name;
+		ends[name[name.length - 1].toLowerCase()] = name;
+	}
+
+	let starter = null;
+
+	for (let i = 0; i < names.length; i++) {
+		let name = names[i];
+		if (!ends[name[0].toLowerCase()]) {
+			starter = name;
+			break;
+		}
+	}
+
+	let i = 1;
+	let ret = [starter];
+	let current = starter;
+	while (i < total) {
+		let name = starts[current[current.length - 1].toLowerCase()];
+		ret.push(name);
+		current = name;
+		i++;
+	}
+
+	return ret;
+}
+
+let input = ['Luis', 'Hector', 'Selena', 'Emmanuel', 'Amish']
+console.log(sorter(input))
