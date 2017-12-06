91de362bcb5c126c0b587832b04eccb4e8fb1eb2
Alejandro Baltra <abaltra@gmail.com>
2017-12-06 18:28:37 -0300

    finished right_sibling

--- right_sibling.js
+++ right_sibling.js
@@ -7,3 +7,42 @@ class Node {
 Each node represents an element of a tree and specifies a list of immediate children (in order from left to right).
 Write a method that, taking in the root node, sets the Right property to the rightmost sibling of a node
 */
+
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.children = [];
+		this.right = null;
+	}
+
+	addChild(node) {
+		this.children.push(node);
+	}
+
+	setRight(node) {
+		this.right = node;
+	}
+}
+
+function set_rights(node, parent=null) {
+	if (!node) return;
+
+	if (parent) {
+		node.setRight(parent.children[parent.children.length - 1]);
+	}
+	for (let i = 0; i < node.children.length; i++) {
+		set_rights(node.children[i], node);
+	}
+}
+
+let tree = new Node('a');
+tree.addChild(new Node('b'));
+tree.addChild(new Node('c'));
+tree.children[0].addChild(new Node('d'));
+tree.children[0].addChild(new Node('e'));
+tree.children[0].addChild(new Node('f'));
+tree.children[1].addChild(new Node('g'));
+tree.children[1].addChild(new Node('h'));
+
+set_rights(tree);
+console.log(tree.children[0].children[0])
