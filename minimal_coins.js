b313c14ad22ad6369f156c6d55326520571ebed0
Alejandro Baltra <abaltra@gmail.com>
2017-12-06 19:34:14 -0300

    minimal coins done

--- minimal_coins.js
+++ minimal_coins.js
@@ -1,3 +1,21 @@
 /*
-Dynamic programming question about coin change, expected result was minimum number of coins with the coin types
+	Dynamic programming question about coin change, expected result was minimum number of coins with the coin types
 */
+
+function minimum_number(denominations, value, map={}) {
+	if (value === 0) return 0;
+
+	if (map[value]) return map[value];
+	let smallest = 100000;
+	for (let i = 0; i < denominations.length; i++) {
+		let coin = denominations[i];
+		if (coin <= value) {
+			let _small = minimum_number(denominations, value - coin, map);
+			smallest = Math.min(_small + 1, smallest);
+		}
+	}
+	map[value] = smallest;
+	return smallest;
+}
+
+console.log(minimum_number([9, 6, 5, 3, 2, 1], 1234));
