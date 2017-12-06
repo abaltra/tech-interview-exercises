4df55de9684b37a2fd0f66b0b688016c890b399a
Alejandro Baltra <abaltra@gmail.com>
2017-12-06 17:17:46 -0300

    preceding words completed

--- preceding_words.js
+++ preceding_words.js
@@ -15,3 +15,28 @@ for k = 2 and w = beta, the result is booking, alpha, beta
 
 Try to keep the space complexity under O(n)
 */
+
+function get_previous_words(source, w, k) {
+	let index = 0;
+	let delayed_index = 0;
+
+	while(index < source.length) {
+		let word = source[index];
+		if (word == w) break;
+
+		index++;
+		if (index > k) delayed_index++;
+	}
+
+	let ret = [];
+	for (let i = delayed_index; i <= index; i++) {
+		ret.push(source[i]);
+	}
+
+	return ret;
+}
+
+let input = ['aaa', 'bbb', 'ccc', 'booking', 'alpha', 'beta', 'gamma'];
+
+console.log(get_previous_words(input, 'booking', 3))
+console.log(get_previous_words(input, 'beta', 2))
